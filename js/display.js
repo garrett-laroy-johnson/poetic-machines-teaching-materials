e1fd449e636f5cee83b54e0b517e7f96403d7f91
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2022-01-31 11:49:20 -0600

    poop

---
+++ js/display.js
@@ -0,0 +1,13 @@
+// let projects = {
+//   about : "https://editor.p5js.org/gjohns13-spectacular/full/fhAKFBn-Y",
+// }
+
+
+function loadDoc(name) {
+
+// let proj = JSON.parse(projects);
+// let projLink =
+//let link = projects[name];
+let iframe = document.getElementById('main-window');
+iframe.setAttribute("src", name)
+}
