889f48a7cb5b4529b9bc39b2692c7209d0994a99
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2022-08-26 14:28:50 -0500

    updating group proj. about complete.

--- js/display.js
+++ js/display.js
@@ -1,3 +1,33 @@
+let state = 0;
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+  var elements = document.getElementById("sidebar").querySelectorAll(".btn");
+  for (e=0;e<elements.length;e++){
+    elements[e].classList.toggle("dark-mode");
+  }
+
+  var butt = document.getElementById("darkToggle");
+
+
+  if (state>1){
+    state == 0;
+  }
+  butt.classList.toggle("btn-light");
+  butt.classList.toggle("btn-dark");
+
+  if (state == 1){
+  butt.innerHTML = "Dark";
+  }
+  else {
+    butt.innerHTML = "Light";
+  }
+
+  state += 1;
+
+}
 
 
 function loadDoc(name, dest, type) {
