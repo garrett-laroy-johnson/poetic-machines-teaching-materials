7eab818c45f20734d22adebcaf28e89975c5dfa5
garrett laroy johnson <garrett.laroy.johnson@gmail.com>
2022-09-04 20:36:57 -0500

    new splash

---
+++ js/cell.js
@@ -0,0 +1,58 @@
+let res;
+let matrix;
+let timestep = 120; // timestep in FPS
+let bg = "white";
+let crawlers = [];
+let crawInit = 1; // initial number of crawlers
+let painted = 0; // number of squares painted
+
+function setup() {
+  noStroke();
+  frameRate(timestep);
+  res = window.innerWidth;
+  createCanvas(window.innerWidth, window.innerHeight).parent("#splash")
+  matrix = new mat(res);
+  matrix.fill();
+  // for (i = 0; i < crawInit; i++) {
+  //   let b = new Crawler(Math.floor(random(res)), Math.floor(random(res)), matrix);
+  //   crawlers.push(b);
+  // }
+  background(bg);
+  textSize(30);
+  text("[click & drag]", width/4, height/2)
+}
+
+function mousePressed(){
+  if (painted<1){
+    background(bg);
+  }
+  let b = new Crawler(Math.floor((mouseX/width)*res),Math.floor((mouseY/width)*res),matrix);
+  crawlers.push(b);
+}
+
+
+function mouseDragged(){
+  let b = new Crawler(Math.floor((mouseX/width)*res),Math.floor((mouseY/width)*res),matrix);
+  crawlers.push(b);
+}
+
+function draw() {
+  matrix.getTotal();
+  if (matrix.total<res*2){noLoop()}
+
+  if (matrix.total<matrix.total){
+  for (i = 0; i < crawlers.length; i++) {
+    crawlers[i].move();
+  }
+} else {
+  for (i = 0; i < crawlers.length; i++) {
+    crawlers[i].move();
+  }
+}
+}
+
+
+function hookToProcessing(){
+  remove();
+  splash += 1;
+}
