50fc15efc05db146d21478274bf9e8d43e947af3
garrett laroy johnson <garrett.laroy.johnson@gmail.com>
2022-09-23 14:19:01 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -6,8 +6,9 @@ let walkers = [];
 let bg = "white";
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight).parent("#splash");
-  for (i=0;i<500;i++){
+  let w = window.innerWidth*3/4;
+  createCanvas(w, window.innerHeight).parent("#splash");
+  for (i=0;i<50;i++){
     let b = new Walker (width/2,height/2);
     walkers.push(b);
   }
@@ -20,15 +21,25 @@ function draw() {
 
   for (a=0;a<walkers.length;a++){
   walkers[a].update();
-  walkers[a].show();
+
+//walkers[a].update();
+  walkers[a].edges();
   }
+
+beginShape()
+curveVertex(walkers[0].pos.x,walkers[0].pos.y)
+  for (a=0;a<walkers.length;a++){
+curveVertex(walkers[a].pos.x,walkers[a].pos.y)
+  }
+  curveVertex(walkers[walkers.length-1].pos.x,walkers[walkers.length-2].pos.y);
+  endShape();
 }
 
 class Walker {
   constructor(x,y) {
     this.pos = createVector(x,y);
     this.vel = p5.Vector.random2D();
-    this.vel.mult(random(3));
+    this.vel.mult(random(4));
 
   }
 
@@ -36,12 +47,22 @@ class Walker {
     let mouse = createVector(mouseX,mouseY);
     //let ran = p5.Vector.random2D();
     this.acc= p5.Vector.sub(mouse,this.pos);
-    this.acc.setMag(0.01);
+    this.acc.setMag(0.05);
 
     this.vel.add(this.acc);
-    this.vel.limit(2);
+    this.vel.limit(4);
     this.pos.add(this.vel);
 
+
+  }
+
+  edges(){
+    if(this.pos.x<=0){
+      this.vel.mult(-1);
+    }
+    if(this.pos.x>=width){
+      this.vel.mult(-1);
+    }
 
   }
 
