7df72ad9378b289ba1e767eee8db06dc19282864
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2022-09-26 17:41:41 -0500

    darkmode cookies

--- js/display.js
+++ js/display.js
@@ -1,57 +1,67 @@
-let state = 0;
-
+let state;
 let splash = 0;
 
-function localSettings (){
- state = localStorage.getItem("state");
+function initDark() {
+  state = getItem('dark');
+  console.log(state);
+  if (state === NaN) {
+    state = 0;
+  }
+  let butt = document.getElementById("darkToggle");
+  if (state == 1) {
+    butt.innerHTML = "Light";
+    bg = "black";
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+    let elements = document.getElementById("sidebar").querySelectorAll(".btn");
+    for (e = 0; e < elements.length; e++) {
+      elements[e].classList.toggle("dark-mode");
+    }
+    butt.classList.toggle("btn-light");
+    butt.classList.toggle("btn-dark");
+  }
 }
 
 function darkMode() {
-  var element = document.body;
+  let element = document.body;
   element.classList.toggle("dark-mode");
-
-  var elements = document.getElementById("sidebar").querySelectorAll(".btn");
-  for (e=0;e<elements.length;e++){
+  let elements = document.getElementById("sidebar").querySelectorAll(".btn");
+  for (e = 0; e < elements.length; e++) {
     elements[e].classList.toggle("dark-mode");
   }
-
-  var butt = document.getElementById("darkToggle");
-
-
-
+  let butt = document.getElementById("darkToggle");
   butt.classList.toggle("btn-light");
   butt.classList.toggle("btn-dark");
-
-  if (state % 2){
-  butt.innerHTML = "Dark";
-  bg = "white";
-  }
-  else {
+  if (state == 1) {
+    butt.innerHTML = "Dark";
+    bg = "white";
+  } else {
     butt.innerHTML = "Light";
     bg = "black";
   }
-
   state += 1;
-  localStorage.setItem("state", state);
+  if (state >= 2) {
+    state = 0;
+  }
+  storeItem('dark', state);
+  console.log(state);
+  let test = getItem('dark');
+  console.log("test test " + test)
 }
-
 
 function loadDoc(name, dest, type) {
-if (splash<1){
-hookToProcessing();
-}
-
-if (dest ==="main-window"){
-window.scrollTo(0,0);
-}
-
+  if (splash < 1) {
+    hookToProcessing();
+  }
+  if (dest === "main-window") {
+    window.scrollTo(0, 0);
+  }
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-     if (this.readyState == 4 && this.status == 200) {
-       document.getElementById(dest).innerHTML =
-       this.responseText;
-     }
-}
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(dest).innerHTML = this.responseText;
+    }
+  }
   xhttp.open("GET", `../html/${name}.html`, true);
   xhttp.send();
 }
